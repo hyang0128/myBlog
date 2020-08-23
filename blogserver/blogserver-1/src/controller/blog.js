adc0d9a4bbcb8cdd3bbcb19f3fb43c65b630f709
hyang <yh1553265252@qq.com>
2020-08-23 17:28:52 +0800

    escape防止sql注入

--- blogserver/blogserver-1/src/controller/blog.js
+++ blogserver/blogserver-1/src/controller/blog.js
@@ -1,31 +1,35 @@
 const {
-    exec
+    exec,
+    escape
 } = require('../db/mysql')
 const getList = (author, keyword) => {
+    author = escape(author)
+    // keyword = escape(keyword)
     let sql = `select id,title,content,createtime,author from blogs where 1=1 `;
     if (author) {
-        sql += `and author='${author}' `
+        sql += `and author=${author} `
     }
     if (keyword) {
-        sql += `and title like '%${keyword}%' `
+        sql += `and title like %'${keyword}'% `
     }
     sql += `order by createtime desc;`
     return exec(sql)
 }
 
 const getDetail = (id) => {
-    let sql = `select * from blogs where id='${id}'`;
+    id = escape(id)
+    let sql = `select * from blogs where id=${id}`;
     return exec(sql).then(rows => {
         return rows[0]
     })
 }
 
 const newBlog = (blogData = {}) => {
-    let title = blogData.title,
-        content = blogData.content,
-        author = blogData.author,
+    let title = escape(blogData.title),
+        content = escape(blogData.content),
+        author = escape(blogData.author),
         createTime = Date.now();
-    let sql = `insert into blogs (title,content,createtime,author) values('${title}','${content}',${createTime},'${author}')`
+    let sql = `insert into blogs (title,content,createtime,author) values(${title},${content},${createTime},${author})`
     return exec(sql).then(insertData => {
         // console.log('insert', insertData)
         return {
@@ -35,9 +39,10 @@ const newBlog = (blogData = {}) => {
 }
 
 const updateBlog = (id, blogData = {}) => {
-    let title = blogData.title,
-        content = blogData.content;
-    let sql = `update blogs set title = '${title}',content='${content}' where id='${id}'`
+    id = escape(id)
+    let title = escape(blogData.title),
+        content = escape(blogData.content);
+    let sql = `update blogs set title = ${title},content=${content} where id=${id}`
     return exec(sql).then(updateData => {
         if (updateData.affectedRows > 0) {
             return true
@@ -46,8 +51,10 @@ const updateBlog = (id, blogData = {}) => {
     })
 }
 
-const deleteBlog = (id,author) => {
-    let sql = `delete from blogs where id='${id}' and author='${author}'`
+const deleteBlog = (id, author) => {
+    id = escape(id)
+    author = escape(author)
+    let sql = `delete from blogs where id=${id} and author=${author}`
     return exec(sql).then(delData => {
         if (delData.affectedRows > 0) {
             return true
--- blogserver/blogserver-1/src/controller/user.js
+++ blogserver/blogserver-1/src/controller/user.js
@@ -1,10 +1,13 @@
 const {
-    exec
+    exec,
+    escape
 } = require('../db/mysql')
 const isLogin = (username, password) => {
-    let sql = `select username,realname from users where username='${username}' and password='${password}'`;
+    username = escape(username)
+    password = escape(password)
+    let sql = `select username,realname from users where username=${username} and password=${password}`;
     return exec(sql).then(rows => {
-        return rows[0]||{}
+        return rows[0] || {}
     })
 }
 
